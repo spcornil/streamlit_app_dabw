b0c4c4c71eb728182d84358bc7a16db2e1b41f34
Sean Cornillie <sean.cornillie@gmail.com>
2023-01-24 17:04:45 -0700

    added picklist

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,12 @@ sl.text('🐔 Hard-Boiled Free-Range Egg')
 sl.text('🥑🍞 Avocado Toast')
 sl.header('🍌🥭 Build your Own Fruit Smoothie 🥝🍇')
 
-fruit_csv = "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+fruit_csv = 'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt'
+
 my_fruit_list = pd.read_csv(fruit_csv)
+my_fruit_list.set_index.('Fruit')
+
+sl.multiselect('Pick some fruits:', list(my_fruit_list.index))
 
 sl.dataframe(my_fruit_list)
+
