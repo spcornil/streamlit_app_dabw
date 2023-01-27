6bf9722fb051bc3ce2c9d87629164c1868e8c98a
Sean Cornillie <sean.cornillie@gmail.com>
2023-01-27 15:56:45 -0700

    converted to function

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,34 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 st.dataframe(fruits_to_show)
 
+################
 ### FruityVice Picker
+#st.header('Fruityvice Fruit Advice')
+#try:
+#    fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
+#    if not fruit_choice:
+#        st.error("Please select a fruit to get information")
+#    else:
+#        fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+#        st.dataframe(fruityvice_normalized)
+#except URLError as e:
+#    st.error()
+#############
+
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())   
+    return fruityvice_normalized
+
 st.header('Fruityvice Fruit Advice')
 try:
     fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
     if not fruit_choice:
         st.error("Please select a fruit to get information")
     else:
-        fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-        st.dataframe(fruityvice_normalized)
+        back_from_function = get_fruityvice_data(fruit_choice)
+        st.dataframe(back_from_function)
 except URLError as e:
     st.error()
 
