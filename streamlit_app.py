5629bee5651ef44f65edc2a436d870bbf669aceb
Sean Cornillie <sean.cornillie@gmail.com>
2023-01-27 15:34:45 -0700

    connected to fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,9 @@ st.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-st.text("Hello from Snowflake:")
-st.text(my_data_row)
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+st.header("The fruit load list contains:")
+st.dataframe(my_data_rows)
 
 
